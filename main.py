9259d28a7ae9aca51ece8a0e0c31962c0332873e
Timothy B <74752830+TIMOTHY1498@users.noreply.github.com>
2024-07-06 21:53:34 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import os, time
+
+# gyatt variable
+kai_cenat = os.listdir()
+files = []
+
+for file in kai_cenat:
+    if file == "main.py":
+        print("stop, don't rizz me. i am the king of gyatts 🤬🤓")
+        continue
+    else:
+        files.append(file)
+        print("Rizz the file, "+file) 
+        
+# kenzi kontol (kontol kenzi hancur)
